28e59221d4d53016bf732b5b5fa3acb10df48fe8
sec05 <spencer.ec09@gmail.com>
2023-03-08 20:38:43 -0500

    Interation 1 of printing dns records

--- main.go
+++ main.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"log"
-	"github.com/google/gopacket/pcap"
+	//"log"
+	//"github.com/google/gopacket/pcap"
 )
 
 func main(){
-	devices, _ := pcap.FindAllDevs()
-	for _, device := range devices{
-		log.Println(device)
-	}
+	Parser()
 }
---
+++ parser.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"log"
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket/pcap"
+	"strconv"
+	"time"
+)
+func Parser()  *string{
+	var (
+		err      	error
+		handle   	*pcap.Handle
+		//InetAddr 	string
+		SrcIP    	string
+		DstIP    	string
+		//eth layers.Ethernet
+	 	ip4 layers.IPv4
+	 	ip6 layers.IPv6
+	 	//tcp layers.TCP
+	 	//udp layers.UDP
+	 	
+		dns layers.DNS
+	)
+	type DnsMsg struct {
+		Timestamp       string
+		SourceIP        string
+		DestinationIP   string
+		DnsQuery        string
+		DnsAnswer       []string
+		DnsAnswerTTL    []string
+		NumberOfAnswers string
+		DnsResponseCode string
+		DnsOpCode       string
+	}
+	handle , err = pcap.OpenLive("\\Device\\NPF_{F7A0656D-948F-4FC0-88F4-113C6332D5E7} Intel(R) 82574L Gigabit Network Connection",22,true,pcap.BlockForever)
+	if err != nil{
+		log.Fatalln("Parser recieved an error: "+err.Error())
+	}
+	defer handle.Close()
+	decoder := gopacket.NewDecodingLayerParser(layers.LayerTypeEthernet, &dns)
+	decodedLayers := make([]gopacket.LayerType, 0,10)
+	for {
+		data, _, err := handle.ReadPacketData()
+		if err != nil {
+			log.Println("Error reading packet data: ", err)
+			continue	
+		}
+	err = decoder.DecodeLayers(data, &decodedLayers)
+	for _,t := range decodedLayers{
+		switch t{
+			case layers.LayerTypeIPv4:
+				SrcIP = ip4.SrcIP.String()
+				DstIP = ip4.DstIP.String()
+			case layers.LayerTypeIPv6:
+				SrcIP = ip6.SrcIP.String()
+				DstIP = ip6.DstIP.String()
+			case layers.LayerTypeDNS:
+				dnsOpCode := int(dns.OpCode)
+				dnsResponseCode := int(dns.ResponseCode)
+				dnsANCount := int(dns.ANCount)
+				if (dnsANCount == 0 && dnsResponseCode > 0) || (dnsANCount > 0) {
+
+					log.Println("————————")
+					log.Println("    DNS Record Detected")
+
+					for _, dnsQuestion := range dns.Questions {
+
+						t := time.Now()
+						timestamp := t.Format(time.RFC3339)
+
+						// Add a document to the index
+						d := DnsMsg{Timestamp: timestamp, SourceIP: SrcIP,
+							DestinationIP:   DstIP,
+							DnsQuery:        string(dnsQuestion.Name),
+							DnsOpCode:       strconv.Itoa(dnsOpCode),
+							DnsResponseCode: strconv.Itoa(dnsResponseCode),
+							NumberOfAnswers: strconv.Itoa(dnsANCount)}
+							log.Println("    DNS OpCode: ", strconv.Itoa(int(dns.OpCode)))
+							log.Println("    DNS ResponseCode: ", dns.ResponseCode.String())
+							log.Println("    DNS # Answers: ", strconv.Itoa(dnsANCount))
+							log.Println("    DNS Question: ", string(dnsQuestion.Name))
+							log.Println("    DNS Endpoints: ", SrcIP, DstIP)
+
+						if dnsANCount > 0 {
+
+							for _, dnsAnswer := range dns.Answers {
+								d.DnsAnswerTTL = append(d.DnsAnswerTTL,strconv.Itoa(int(dnsAnswer.TTL)))
+								if dnsAnswer.IP.String() != "<nil>" {
+									log.Println("    DNS Answer: ", dnsAnswer.IP.String())
+									d.DnsAnswer = append(d.DnsAnswer, dnsAnswer.IP.String())
+								}
+							}
+
+							}	}
+		}
+	}
+}
+	}
+}
