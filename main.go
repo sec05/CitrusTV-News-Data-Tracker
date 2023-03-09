018cec4e6fdca63c65c8708b7fce6a849390c2ff
sec05 <spencer.ec09@gmail.com>
2023-03-08 20:41:26 -0500

    printing all devices

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	//"log"
-	//"github.com/google/gopacket/pcap"
+	"log"
+	"github.com/google/gopacket/pcap"
 )
 
 func main(){
+	p, _:= pcap.FindAllDevs()
+	log.Println(p)
 	Parser()
 }
